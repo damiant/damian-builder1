a6951bb43858e3613f71f9afaafe7354e827c296
Damian Tarnawsky <damian@builder.io>
2024-12-19 10:43:11 -0800

    feat: resize

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,21 @@
+"use client"
 import Image from "next/image";
+import { useEffect } from "react";
+
+function resize(height: string) {
+  const e = document.getElementById('page');
+  if (e) {
+    e.style.height = height;
+  }
+}
 
 export default function Home() {
-  return (
+  useEffect(() => {
+    setTimeout(() => { resize('200px')}, 50);
+    setTimeout(() => { resize('300px')}, 100);
+    setTimeout(() => { resize('1300px')}, 150);
+  });
+  return (    
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
         <Image
