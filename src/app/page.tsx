45b446a85d0811b9dd96581ac37d8ad3f00b53c4
Damian Tarnawsky <damian@builder.io>
2024-12-19 12:17:24 -0800

    fix: log content

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -29,6 +29,7 @@ export default async function Home() {
       },
     })
     .toPromise();
+  console.log('content', content);
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center  p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <RenderBuilderContent content={content} model="page"></RenderBuilderContent>
