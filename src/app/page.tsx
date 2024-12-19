b57f0eb3932f49dbe6fbe04838eb12856d746192
Damian Tarnawsky <damian@builder.io>
2024-12-19 12:20:35 -0800

    fix: test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -20,7 +20,7 @@ export default async function Home() {
     setTimeout(() => { resize('200px') }, 50);
     setTimeout(() => { resize('300px') }, 100);
     setTimeout(() => { resize('1300px') }, 150);
-  }
+  
   //});
   const content = await builder
     .get("page", {
@@ -30,6 +30,7 @@ export default async function Home() {
     })
     .toPromise();
   console.log('content', content);
+  }
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center  p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <RenderBuilderContent content={content} model="page"></RenderBuilderContent>
