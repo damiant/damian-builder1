d9d8bf9725f97ccc4f86e34dea90e759319a22de
Damian Tarnawsky <damian@builder.io>
2025-01-10 14:01:05 -0800

    fix: switch for dev

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -21,7 +21,7 @@ export default async function Home() {
       <div className="page">
         <RenderBuilderContent content={content} model="page"></RenderBuilderContent>
       </div>
-      {process.env.IS_DEVELOPMENT !== 'true' &&
+      {process.env.IS_DEVELOPMENT === 'true' &&
         <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center  p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
           <div className="flex gap-4 items-center flex-col sm:flex-row">
             <a
