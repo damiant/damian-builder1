9e1e997ab47b8c294b2646429f3e199593d86b0e
Damian Tarnawsky <damian@builder.io>
2025-01-10 12:09:00 -0800

    fix: change model to Home

--- src/app/[...page]/page.tsx
+++ src/app/[...page]/page.tsx
@@ -11,7 +11,7 @@ interface PageProps {
 }
 
 export default async function Page(props: PageProps) {
-  const builderModelName = "page";
+  const builderModelName = "Home";
 
   const content = await builder
     // Get the page content from Builder with the specified options
