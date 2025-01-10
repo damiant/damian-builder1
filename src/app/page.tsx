8515815d7eb2c3e48e0accf54f1000b1ef6ffc00
Damian Tarnawsky <damian@builder.io>
2025-01-10 14:04:12 -0800

    fix: add buttons

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -25,27 +25,21 @@ export default async function Home() {
         <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center  p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
           <div className="flex gap-4 items-center flex-col sm:flex-row">
             <a
-              className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-              href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+              className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background min-w-44 gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
+              href="https://www.builder.io/blog"
               target="_blank"
               rel="noopener noreferrer"
             >
-              <Image
-                className="dark:invert"
-                src="/vercel.svg"
-                alt="Vercel logomark"
-                width={20}
-                height={20}
-              />
-              Deploy now
+              
+              Blog
             </a>
             <a
               className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-              href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+              href="https://www.builder.io/c/docs/developers"
               target="_blank"
               rel="noopener noreferrer"
             >
-              Read Docs
+              Docs
             </a>
           </div>
         </div>
