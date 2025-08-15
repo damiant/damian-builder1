5939d3076a2e8d34913d706d9c723d7cad675f78
Damian Tarnawsky <damian@builder.io>
2025-08-15 09:58:47 -0700

    update example code

--- src/app/[...page]/page.tsx
+++ src/app/[...page]/page.tsx
@@ -1,6 +1,7 @@
 import { builder } from "@builder.io/sdk";
 import { RenderBuilderContent } from "../../components/builder";
 
+
 // Builder Public API Key set in .env file
 builder.init(process.env.NEXT_PUBLIC_BUILDER_API_KEY!);
 
@@ -11,12 +12,12 @@ interface PageProps {
 }
 
 export default async function Page(props: PageProps) {
-  const builderModelName = "page";  
+  const model = "page";
   const content = await builder
     // Get the page content from Builder with the specified options
-    .get(builderModelName, {
-      userAttributes: {
-        //cachebust: process.env.IS_DEVELOPMENT === "true") ? true : false,
+    .get(model, {
+      cachebust: process.env.IS_DEVELOPMENT === "true", // Cache busting in development mode
+      userAttributes: {        
         // Use the page path specified in the URL to fetch the content
         urlPath: "/" + ((await props?.params)?.page?.join("/") || ""),
       },
@@ -24,10 +25,13 @@ export default async function Page(props: PageProps) {
     // Convert the result to a promise
     .toPromise();
 
-  return (
-    <>
-      {/* Render the Builder page */}
-      <RenderBuilderContent content={content} model={builderModelName} />
-    </>
-  );
+  
+    return (
+      <>
+        {/* Render the Builder page */}
+        
+        <RenderBuilderContent content={content} model={model}></RenderBuilderContent>
+      </>
+    );
+
 }
