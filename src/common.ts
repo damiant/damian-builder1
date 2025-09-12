0c2531f435222031d836d3552866158b67f2b8a5
Damian Tarnawsky <damian@builder.io>
2025-09-12 10:38:55 -0700

    fix: test staleCacheSeconds 0

--- src/common.ts
+++ src/common.ts
@@ -2,7 +2,7 @@ import { GetContentOptions } from "@builder.io/sdk";
 
 export const commonOptions: GetContentOptions = {
   cacheSeconds: 4,
-  staleCacheSeconds: 4,
+  staleCacheSeconds: 0,
   fetchOptions: {
     headers: {
       "x-builder-no-stale-cache": "true", // Wont work until deployed
