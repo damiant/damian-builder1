7a2b60bd36ddb03f9c3807ee4ec2b2237a26ab9e
Damian Tarnawsky <damian@builder.io>
2025-09-12 10:35:19 -0700

    feat: test cache

--- src/common.ts
+++ src/common.ts
@@ -1,10 +1,12 @@
 import { GetContentOptions } from "@builder.io/sdk";
 
 export const commonOptions: GetContentOptions = {
-  cacheSeconds: 5,
+  cacheSeconds: 4,
+  staleCacheSeconds: 4,
   fetchOptions: {
     headers: {
-      "x-builder-no-stale-cache": "true",
+      "x-builder-no-stale-cache": "true", // Wont work until deployed
+      "x-builder-custom-fastly-maxage": 4
     },
   },
 };
