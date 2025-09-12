96c5399eb8867e52960fb0a882418d06d05039d0
Damian Tarnawsky <damian@builder.io>
2025-09-12 13:16:56 -0700

    fix; test 2 secs

--- src/common.ts
+++ src/common.ts
@@ -6,9 +6,8 @@ export const commonOptions: GetContentOptions = {
   fetchOptions: {
     //cache: "no-store", // Turn off Nextjs fetch caching https://nextjs.org/docs/app/guides/caching#opting-out-1
     cache: 'force-cache', // NextJS force caching
-    next: { revalidate: 30}, // Revalidate this page every 30 seconds
+    next: { revalidate: 2}, // Revalidate this page every 30 seconds
     headers: {
-      "Cache-Control": "max-age=30", // This doesnt work for 30 seconds and remains cached forever?
       "x-builder-no-stale-cache": "true", // Wont work until deployed
       "x-builder-custom-fastly-maxage": 0,
     },
