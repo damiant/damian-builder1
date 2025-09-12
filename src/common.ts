302554dd73e5eb1e442aa558b2345f453c8c9e02
Damian Tarnawsky <damian@builder.io>
2025-09-12 13:06:40 -0700

    fix: move header for cache control

--- src/common.ts
+++ src/common.ts
@@ -5,8 +5,8 @@ export const commonOptions: GetContentOptions = {
   staleCacheSeconds: 0,
   fetchOptions: {
     //cache: "no-store", // Turn off Nextjs fetch caching https://nextjs.org/docs/app/guides/caching#opting-out-1
-    'Cache-Control': "max-age=30", // This doesnt work for 30 seconds and remains cached forever?
     headers: {
+      "Cache-Control": "max-age=30", // This doesnt work for 30 seconds and remains cached forever?
       "x-builder-no-stale-cache": "true", // Wont work until deployed
       "x-builder-custom-fastly-maxage": 0,
     },
