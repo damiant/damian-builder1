90bedc4c77470119f44b11f82fd1d2eb2b4407df
Damian Tarnawsky <damian@builder.io>
2025-09-13 06:32:26 -0700

    fix: remove no stale cache

--- src/common.ts
+++ src/common.ts
@@ -5,11 +5,11 @@ export const commonOptions: GetContentOptions = {
   staleCacheSeconds: 0,
   fetchOptions: {
     //cache: "no-store", // Turn off Nextjs fetch caching https://nextjs.org/docs/app/guides/caching#opting-out-1
-    cache: 'force-cache', // NextJS force caching
-    next: { revalidate: 2}, // Revalidate this page every 30 seconds
-    headers: {
-      "x-builder-no-stale-cache": "true", // Wont work until deployed
-      "x-builder-custom-fastly-maxage": 0,
-    },
+    cache: 'force-cache', // NextJS force to use caching
+    next: { revalidate: 2}, // Revalidate if 2 seconds or longer
+    // headers: {
+    //   "x-builder-no-stale-cache": "true", // Wont work until deployed
+    //   "x-builder-custom-fastly-maxage": 0,
+    // },
   },
 };
