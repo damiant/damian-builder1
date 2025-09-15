6f3f221adcce6862504525a3d671e08a9451f0a2
Damian Tarnawsky <damian@builder.io>
2025-09-15 12:37:00 -0700

    test: force-cache

--- src/common.ts
+++ src/common.ts
@@ -5,7 +5,7 @@ export const commonOptions: GetContentOptions = {
   staleCacheSeconds: 0,
   fetchOptions: {
     //cache: "no-store", // Turn off Nextjs fetch caching https://nextjs.org/docs/app/guides/caching#opting-out-1
-    cache: 'force-cache', // NextJS force to use caching
+    //cache: 'force-cache', // NextJS force to use caching
     next: { revalidate: 2}, // Revalidate if 2 seconds or longer
     // headers: {
     //   "x-builder-no-stale-cache": "true", // Wont work until deployed
