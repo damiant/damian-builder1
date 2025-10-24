9f7865dcd9d60ecde4d0835c8c41bbb56ba2cd48
Damian Tarnawsky <damian@builder.io>
2025-10-24 06:54:39 -0700

    fix: test

--- src/common.ts
+++ src/common.ts
@@ -4,9 +4,9 @@ export const commonOptions: GetContentOptions = {
   //cacheSeconds: 2,
   //staleCacheSeconds: 0,
   fetchOptions: {
-    //cache: "no-store", // Turn off Nextjs fetch caching https://nextjs.org/docs/app/guides/caching#opting-out-1
+    cache: "no-store", // Turn off Nextjs fetch caching https://nextjs.org/docs/app/guides/caching#opting-out-1
     //cache: 'force-cache', // NextJS force to use caching
-    next: { revalidate: 2}, // Revalidate if 2 seconds or longer
+    next: { revalidate: 2 }, // Revalidate if 2 seconds or longer
     // headers: {
     //   "x-builder-no-stale-cache": "true", // Wont work until deployed
     //   "x-builder-custom-fastly-maxage": 0,
